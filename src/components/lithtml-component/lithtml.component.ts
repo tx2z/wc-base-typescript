9c744697041ce71c67a92194adcbd95085f7424e
tx2z <tx2z@caosmental.com>
2019-10-20 16:35:53 +0200

    add lit-html based component

--- src/components/lithtml-component/lithtml.component.ts
+++ src/components/lithtml-component/lithtml.component.ts
@@ -1,49 +1,37 @@
-// import * as interfaces from './lithtml.component.interfaces';
 import { html, render } from 'lit-html';
-import prepareTemplate from '../../helpers';
-import htmlTemplate from './lithtml.component.html';
+import * as interfaces from './lithtml.component.interfaces';
 
 /** The LithtmlComponent web component */
 export default class LithtmlComponent extends HTMLElement {
   private shadow: ShadowRoot;
+  private templateVariables: interfaces.TemplateVariables = {
+    hello: 'Hello There!',
+  };
 
   constructor() {
     super();
 
-    // Add the main template to the component
-    const templateElement = document.createElement('template');
-
-    // Prepare template
-    const variables = {
-      hello: 'Hello There!',
-    };
-    templateElement.innerHTML = prepareTemplate(htmlTemplate, variables, '');
-
-    // Attach template content to the shadow dom
     this.shadow = this.attachShadow({ mode: 'closed' });
-    this.shadow.appendChild(templateElement.content.cloneNode(true));
 
-    this.addEventListeners();
+    const lithtmlTemplate = (data: interfaces.TemplateVariables) =>
+      html`
+        ${data.hello}
+        <br />
+        <button @click=${this.buttonClick}>Test button</button>
+        <p id="testMessage"></p>
+      `;
+
+    render(lithtmlTemplate(this.templateVariables), this.shadow, { eventContext: this });
   }
 
-  /**
-   * Add EventListeners to the Component
-   */
-  private addEventListeners() {
-    const lithtmlComponentElement = this.shadow;
+  public buttonClick() {
+    const testMessage = this.shadow.getElementById('testMessage') as HTMLElement;
 
-    lithtmlComponentElement.addEventListener(
-      'click',
-      event => {
-        const target = event.target as HTMLElement;
+    const messageTemplate = () =>
+      html`
+        You click the button!
+      `;
 
-        if (target.id === 'testButton') {
-          event.preventDefault();
-          const testMessage = this.shadow.getElementById('testMessage') as HTMLElement;
-          testMessage.innerHTML = 'You click the button!';
-        }
-      },
-      false
-    );
+    render(messageTemplate(), testMessage);
   }
 }
